d8f49cacf7760d334fda5fdf95a250a24cf1d574
JeHyeuk <JeHyeuk.Lee@hyundai-kefico.com>
2026-04-28 16:38:52 +0900

    [labwons.manager]

--- src/pylabwons_stub/core/build/stock.py
+++ src/pylabwons_stub/core/build/stock.py
@@ -25,13 +25,20 @@ class Stock(Ticker):
     #         )
     #     return
 
-    def release(self, path:Union[str, Path]=None):
+    def __tech__(self):
         tech = {"date": self.ohlcv.index.strftime("%Y-%m-%d").tolist()}
         for col in self.ohlcv:
-            tech[col] = self.ohlcv[col].tolist()
+            tech[col] = round(self.ohlcv[col], 2).tolist()
+            if col.startswith('trend'):
+                tech[f'{col}_dev'] = round(2 * (self.ohlcv['close'] - self.ohlcv[col]).std(), 2)
+        return tech
 
+
+    def release(self, path:Union[str, Path]=None):
         if path is None:
             path = PATH.DATA / f'release/stock'
+
+        tech = self.__tech__()
         file = path / f'{self.ticker}.html'
         file.parent.mkdir(parents=True, exist_ok=True)
         with open(file=file, mode='w', encoding='utf-8') as file:
@@ -41,7 +48,7 @@ class Stock(Ticker):
                     .render({
                     "title": f"LAB￦ONS: {self.snapshot['name']}({self.ticker})",
                     "ticker": self.ticker,
-                    "tech": dumps(tech),
+                    "tech": dumps(tech).replace("NaN", "null"),
                 })
             )
         return
