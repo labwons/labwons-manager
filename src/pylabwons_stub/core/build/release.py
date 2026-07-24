098128b027df19c0a51470710e460d40598b7a91
JeHyeuk <JeHyeuk.Lee@hyundai-kefico.com>
2026-07-24 15:46:20 +0900

    [labwons.manager]

--- src/pylabwons_stub/core/build/release.py
+++ src/pylabwons_stub/core/build/release.py
@@ -33,7 +33,7 @@ class Release(Baseline):
     @property
     def build_note(self) -> str:
         date = str(self.logger)[:10]
-        note = f"[{ID}]\n| labwons.com"
+        note = f"[{self.ID}]\n| https://labwons.com"
         for line in str(self.logger).splitlines():
             if not line.startswith(date):
                 continue
